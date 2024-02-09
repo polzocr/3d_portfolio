d30658eb679b9f234ea16112815ccdee77b9c216
Polzocr <micro.cpt.62@outlook.fr>
2024-02-09 15:01:56 +0100

    animation skills

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,7 @@ import { ComputersCanvas } from './canvas'
 const Hero = () => {
   return (
     <section className='relative w-full h-screen mx-auto'> 
-      <div className={`${styles.paddingX} absolute inset-0  lg:top-[160px] top-[100px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
+      <div className={`${styles.paddingX} absolute inset-0  lg:top-[220px] top-[100px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
         <div className='flex flex-col justify-center items-center mt-5'>
           <div className='w-5 h-5 rounded-full bg-[#00E8EE]'/>
           <div className='w-1 sm:h-80 h-40 bg-gradient-to-r from-[#00E8EE]'/>
@@ -28,7 +28,7 @@ const Hero = () => {
       </div>
       {/* <ComputersCanvas /> */}
 
-      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
+      <div className='absolute xs:bottom-5 bottom-20 w-full flex justify-center items-center'>
         <a href='#about'>
           <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
             <motion.dev
--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -7,14 +7,14 @@ import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
 
 
-const SkillCard = ({name,icon}) => {
+const SkillCard = ({name,icon, index}) => {
   return (
-    <div className="flex flex-col dark:hover:bg-gray-700  dark:bg-gray-200 border-gray-800 dark:border-white border-4 rounded-lg p-3 sm:m-2 m-4 transition duration-500 ease-in-out hover:bg-gray-800 hover:text-gray-200 transform hover:-translate-y-1 hover:scale-10 items-center justify-center">
-      <img alt={name}  width="64" height="64"  data-nimg="1" className="lg:h-20 lg:w-20 sm:h-10 sm:w-10 fill-current hover:opacity-50" src={icon}></img>
+    <motion.div variants={fadeIn("right","spring", 0.5*index, 0.5)} className="flex flex-col dark:hover:bg-gray-700  dark:bg-gray-200 border-gray-800 dark:border-white border-4 rounded-lg p-3 sm:m-2 m-4 hover:bg-gray-800 hover:text-gray-200 transform hover:-translate-y-1 hover:scale-10 items-center justify-center">
+      <img alt={name}  width="64" height="64"  className="lg:h-20 lg:w-20 sm:h-10 sm:w-10 fill-current hover:opacity-50" src={icon}></img>
       <div className="flex flex-col items-start mt-4 ">
         <h4 className="text-xl font-semibold pt-3">{name}</h4>
       </div>
-    </div>
+    </motion.div>
   )
 }
 
@@ -22,7 +22,7 @@ const Tech = () => {
   return (
     <>
       <motion.div variants={textVariant()}> 
-        <p className={styles.sectionSubText}>Compétences</p>
+        <p className={styles.sectionSubText}>Technologies</p>
         <h2 className={styles.sectionHeadText}>Compétences.</h2>
       </motion.div>
       {/* <div className='flex flex-row flex-wrap justify-center gap-10'>
@@ -36,8 +36,8 @@ const Tech = () => {
 
     <div className="flex items-center justify-center pb-20 pt-10">
       <div className="grid xl:grid-cols-6 md:grid-cols-3 lg:grid-cols-5 grid-cols-2 lg:gap-6 gap-3 max-w-6xl">
-        {technologies.map((technology) => (
-          <SkillCard name={technology.name} icon={technology.icon} key={technology.name}/>
+        {technologies.map((technology, index) => (
+          <SkillCard name={technology.name} icon={technology.icon} index = {index} key={technology.name}/>
         ))}
 
       </div>
