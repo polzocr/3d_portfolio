6d20dfd54e99162d3a49c8f3c0e31e19040043b1
Polzocr <micro.cpt.62@outlook.fr>
2024-02-09 11:23:37 +0100

    hero ok

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,5 @@
 import {motion} from 'framer-motion'
+import { localisation } from '../assets' 
 
 import {styles} from '../styles'
 import { ComputersCanvas } from './canvas'
@@ -7,7 +8,7 @@ import { ComputersCanvas } from './canvas'
 const Hero = () => {
   return (
     <section className='relative w-full h-screen mx-auto'> 
-      <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
+      <div className={`${styles.paddingX} absolute inset-0  lg:top-[160px] top-[100px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
         <div className='flex flex-col justify-center items-center mt-5'>
           <div className='w-5 h-5 rounded-full bg-[#00E8EE]'/>
           <div className='w-1 sm:h-80 h-40 bg-gradient-to-r from-[#00E8EE]'/>
@@ -15,6 +16,14 @@ const Hero = () => {
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>Hey ! C'est<span className='text-[#00E8EE]'> Paul</span></h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>Je suis développeur <span className='text-[#00E8EE]'> Javascript</span> <br className='sm:block hidden'/>aussi bien <span className='text-[#00E8EE]'> FrontEnd</span> que  <span className='text-[#00E8EE]'> BackEnd</span></p>
+          <div className='mt-5 flex items-center gap-4'>
+            <img src={localisation} alt="localisation-logo" className='w-5 invert'/>
+            <p className='text-[#00E8EE] text-2xl'> Paris, Ile de france</p>
+          </div>
+          <div className=' mt-8 sm:mt-14  flex items-center gap-5 '>
+            <a href="/" className='bg-white py-3 px-8 rounded-xl outline-none w-fit text-primary font-bold shadow-md shadow-primary hover:bg-[#00E8EE]'>Voir CV</a>
+            <a href="#contact" className='bg-white py-3 px-8 rounded-xl outline-none w-fit text-primary font-bold shadow-md shadow-primary hover:bg-[#00E8EE]'>Contact</a>
+          </div>
         </div>
       </div>
       {/* <ComputersCanvas /> */}
