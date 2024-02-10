1ca2f1cb2152031e0805a378c70923241576f4ba
Polzocr <micro.cpt.62@outlook.fr>
2024-02-10 18:17:57 +0100

    traduction projets text

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -71,8 +71,8 @@ const Works = () => {
   return (
     <>
       <motion.div variants={textVariant()}>
-        <p className={`${styles.sectionSubText} `}>My work</p>
-        <h2 className={`${styles.sectionHeadText}`}>Projects.</h2>
+        <p className={`${styles.sectionSubText} `}>Mon Travail</p>
+        <h2 className={`${styles.sectionHeadText}`}>Projets.</h2>
       </motion.div>
 
       <div className='w-full flex'>
@@ -80,11 +80,11 @@ const Works = () => {
           variants={fadeIn("", "", 0.1, 1)}
           className='mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]'
         >
-          Following projects showcases my skills and experience through
-          real-world examples of my work. Each project is briefly described with
-          links to code repositories and live demos in it. It reflects my
-          ability to solve complex problems, work with different technologies,
-          and manage projects effectively.
+          Les projets suivants mettent en valeur mes compétences et mon expérience à travers
+          des exemples concrets de mon travail. Chaque projet est brièvement décrit avec
+          des liens vers des référentiels de code et des démos en direct. Cela reflète ma
+          capacité à résoudre des problèmes complexes, à travailler avec différentes technologies,
+          et gérer efficacement les projets.
         </motion.p>
       </div>
 
